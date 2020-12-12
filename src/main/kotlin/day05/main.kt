15817840ca92abb4cbd77e586eb3302858e0d3e3
Johan Lindskogen <johan.lindskogen@gmail.com>
2020-12-12 01:27:27 +0100

    Add day 5

---
+++ src/main/kotlin/day05/main.kt
@@ -0,0 +1,70 @@
+package day05
+
+import java.io.File
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+import java.math.BigInteger
+import java.security.MessageDigest
+
+fun md5(input:String): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+}
+
+
+@ExperimentalTime
+fun main() {
+    val (value, elapsed) = measureTimedValue {
+        val input = "ojvtpuvg"
+        println("Part 1: ${solve1(input)}")
+        println("Part 2: ${solve2(input)}")
+    }
+
+    println(elapsed)
+}
+
+fun solve1(s: String): String {
+    var st = ""
+
+    var n = 0
+
+    while (st.length < 8) {
+        val res = md5(s+n)
+        if (res.startsWith("00000")) {
+            st += res[5]
+        }
+        n++
+    }
+
+    return st
+}
+
+
+fun solve2(s: String): String {
+    val st = "_".repeat(8).toCharArray()
+    var found = 0
+
+    var n = 0
+
+    while (found < 8) {
+        val res = md5(s+n)
+        if (res.startsWith("00000")) {
+            try {
+                val index = ("" + res[5]).toInt()
+
+                if (index < 8 && st[index] == '_') {
+
+                    st[index] = res[6]
+                    found++
+
+                }
+            } catch (e: NumberFormatException) {
+
+            }
+        }
+        n++
+    }
+
+    return st.joinToString("")
+}
